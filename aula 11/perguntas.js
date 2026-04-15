bcfbabba213f86d1133d0aef69cf222ff7ceaeb0
DEVEduardoiensen <dev.eduardoiensen@gmail.com>
2026-04-15 16:15:34 -0300

    .

---
+++ aula 11/perguntas.js
@@ -0,0 +1,17 @@
+criar_cartao(
+    "Programação",
+    "Onde fica o link do CSS?",
+    "No head"
+)
+
+criar_cartao(
+    "Futebol",
+    "Qual o campeão da Libertadores de 2024?",
+    "Botafogo"
+)
+
+criar_cartao(
+    "Futebol",
+    "Qual o campeão da Libertadores de 2024?",
+    "Botafogo"
+)
